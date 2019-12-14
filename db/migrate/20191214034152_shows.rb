307bca93afd6a2170b0096b2aa6110ec4aee795f
Nethelbert <gilblasse@gmail.com>
2019-12-14 04:18:58 +0000

    Automatically backed up by Learn

--- db/migrate/20191214034152_shows.rb
+++ db/migrate/20191214034152_shows.rb
@@ -3,6 +3,7 @@ class Shows < ActiveRecord::Migration[5.2]
     create_table :shows do |t|
       t.string :name
       t.string :day 
+      t.string :season
       t.string :network 
       t.integer :rating
     end
