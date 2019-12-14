80f5ab5b22cb8104839ce27f9c99d8535fb8027f
Nethelbert <gilblasse@gmail.com>
2019-12-14 03:45:07 +0000

    Automatically backed up by Learn

--- db/migrate/20191214034152_shows.rb
+++ db/migrate/20191214034152_shows.rb
@@ -1,5 +1,16 @@
 class Shows < ActiveRecord::Migration[5.2]
   def change
-    
+    create :shows dp |t|
+      t.string :name
+      t.string :day 
+      t.string :network 
+      t.integer :rating
+    end
   end
 end
+
+
+    expect(@rick_and_morty.name).to eq("Rick and Morty")
+      expect(@rick_and_morty.day).to eq("Thursday")
+      expect(@rick_and_morty.network).to eq("Adult Swim")
+      expect(@rick_and_morty.rating).to eq(10)
